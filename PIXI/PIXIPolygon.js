39eef0247a6aa32e271029f439d9bbb03b152aa4
Michael Enion <caewok_2000@yahoo.com>
2022-12-03 09:44:39 -0800

    Add clipperClip method to PIXI.Polygon

--- PIXI/PIXIPolygon.js
+++ PIXI/PIXIPolygon.js
@@ -52,6 +52,12 @@ export function registerPIXIPolygonMethods() {
 
   // ----- Methods ----- //
 
+  Object.defineProperty(PIXI.Polygon.prototype, "clipperClip", {
+    value: clipperClip,
+    writable: true,
+    configurable: true
+  });
+
   Object.defineProperty(PIXI.Polygon.prototype, "close", {
     value: function() {
       if ( this.isClosed ) return;
@@ -176,6 +182,27 @@ function centroid() {
   outPoint.y *= areaMult;
 
   return outPoint;
+}
+
+/**
+ * Clip a polygon with another.
+ * Union, Intersect, diff, x-or
+ * @param {PIXI.Polygon} poly   Polygon to clip against this one.
+ * @param {object} [options]
+ * @param {ClipperLib.ClipType} [options.cliptype]  Type of clipping
+ * @return [ClipperLib.Paths[]] Array of Clipper paths
+ */
+function clipperClip(poly, { cliptype = ClipperLib.ClipType.ctUnion } = {}) {
+  const subj = this.toClipperPoints();
+  const clip = poly.toClipperPoints();
+
+  const solution = new ClipperLib.Paths();
+  const c = new ClipperLib.Clipper();
+  c.AddPath(subj, ClipperLib.PolyType.ptSubject, true); // True to be considered closed
+  c.AddPath(clip, ClipperLib.PolyType.ptClip, true);
+  c.Execute(cliptype, solution);
+
+  return solution;
 }
 
 /**
