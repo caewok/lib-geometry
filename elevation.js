080a1b7b89876cd160030b717912d13b3fb9c2c7
Michael Enion <caewok_2000@yahoo.com>
2023-06-11 11:50:25 -0700

    Update prone status check to v11
    Uses a statuses set instead of flags.

--- elevation.js
+++ elevation.js
@@ -223,11 +223,9 @@ function setTokenBottomE(value) { this.elevationE = value; }
  */
 function tokenTopE() {
   const proneStatusId = CONFIG.GeometryLib.proneStatusId;
-  const isProne = (proneStatusId !== "" && this.actor)
-    ? this.actor.effects.some(e => e.getFlag("core", "statusId") === proneStatusId)
-    : game.modules.get(MODULE_KEYS.LEVELSAUTOCOVER.ID)?.active
-    && this.document.flags?.[MODULE_KEYS.LEVELSAUTOCOVER.ID]?.[MODULE_KEYS.LEVELSAUTOCOVER].DUCKING;
-
+  const isProne = (proneStatusId !== "" && this.actor && this.actor.statuses.has(proneStatusId))
+    || (game.modules.get(MODULE_KEYS.LEVELSAUTOCOVER.ID)?.active
+    && this.document.flags?.[MODULE_KEYS.LEVELSAUTOCOVER.ID]?.[MODULE_KEYS.LEVELSAUTOCOVER].DUCKING);
   const heightMult = isProne ? CONFIG.GeometryLib.proneMultiplier : 1;
   return this.bottomE + (this.losHeight * heightMult);
 }
