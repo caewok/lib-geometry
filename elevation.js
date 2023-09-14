bf994b21e95cb4f7c3608b6a56e22c9bc021640e
Michael Enion <caewok_2000@yahoo.com>
2023-09-14 08:55:45 -0400

    Fix tile mesh elevation getting set incorrectly
    Avoid overwriting tile.document.elevation if it is undefined.

--- elevation.js
+++ elevation.js
@@ -381,7 +381,8 @@ function preUpdateWallHook(wallD, data, _options, _userId) {
  * Monitor tiles drawn to canvas and sync elevation.
  */
 function drawTileHook(tile) {
-  if ( tile.document.elevation !== tile.elevationE ) tile.document.elevation = tile.elevationE;
+  if ( typeof tile.document.elevation !== "undefined"
+     && tile.document.elevation !== tile.elevationE ) tile.document.elevation = tile.elevationE;
 }
 
 
