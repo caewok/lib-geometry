7c0d3cc1fc9277d5b1b8c7d75897497622bb2a8e
Michael Enion <caewok_2000@yahoo.com>
2025-02-18 11:36:31 -0800

    Fix undefined error

--- Edge.js
+++ Edge.js
@@ -32,8 +32,8 @@ PATCHES.CANVAS_EDGES = {};
  */
 function getElevation() {
   const e = this._elevation ??= { a: { top: null, bottom: null }, b: { top: null, bottom: null }};
-  if ( this.object instanceof Wall ) _setWallElevation(this.object);
-  else if ( this.object instanceof Region ) _setRegionElevation(this.object, this);
+  if ( this.object instanceof Wall ) _setWallElevation(this.object, e);
+  else if ( this.object instanceof Region ) _setRegionElevation(this.object, this, e);
   return e;
 }
 
@@ -42,12 +42,12 @@ function getElevation() {
  * @param {Wall} wall
  * @returns {EdgeElevation}
  */
-function _setWallElevation(wall) {
+function _setWallElevation(wall, e) {
   let top = wall.topE;
   let bottom = wall.bottomE;
   if ( !isFinite(top) ) top = null;
   if ( !isFinite(bottom) ) bottom = null;
-  const { a, b } = this._elevation;
+  const { a, b } = e;
   a.top = top;
   a.bottom = bottom;
   b.top = top;
@@ -59,15 +59,15 @@ function _setWallElevation(wall) {
  * @param {Region} region
  * @param {Edge} edge       Edge for which the elevation is applied; needed for ramps
  */
-function _setRegionElevation(region, edge) {
+function _setRegionElevation(region, edge, e) {
   const TM = MODULE_KEYS.TERRAIN_MAPPER;
   let top = region.document.elevation.top;
   if ( TM.ACTIVE && region[TM.ID].isElevated ) {
-    if ( region[TM.ID].isRamp ) return _setRampElevation(region, edge);
+    if ( region[TM.ID].isRamp ) return _setRampElevation(region, edge, e);
     top = region[TM.ID].plateauElevation;
   }
   const bottom = region.document.elevation.bottom;
-  const { a, b } = this._elevation;
+  const { a, b } = e;
   a.top = top;
   a.bottom = bottom;
   b.top = top;
@@ -79,11 +79,11 @@ function _setRegionElevation(region, edge) {
  * @param {Region} region
  * @param {Edge} edge      One edge of the region's polygon(s)
  */
-function _setRampElevation(region, edge) {
+function _setRampElevation(region, edge, e) {
   // Assumes TM is active.
   const TM = MODULE_KEYS.TERRAIN_MAPPER;
   const bottom = region.document.elevation.bottom;
-  const { a, b } = this._elevation;
+  const { a, b } = e;
   a.top = region[TM.ID].elevationUponEntry(edge.a);
   a.bottom = bottom;
   b.top = region[TM.ID].elevationUponEntry(edge.b);
