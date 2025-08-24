c9442da761f1e971c0cda2bc3ccbe04f6db3bd19
Michael Enion <caewok_2000@yahoo.com>
2025-08-24 16:21:54 -0700

    Correct cannot access before initialization error

--- CutawayPolygon.js
+++ CutawayPolygon.js
@@ -101,15 +101,15 @@ export class CutawayPolygon extends PIXI.Polygon {
     const steps2d = steps.map(step => this._to2d(step));
     if ( isHole ) {
       // Remove duplicates. Remember points are flat arrays.
-      if ( this.points[0] === stepPts[0].x && this.points[1] === stepPts[0].y ) stepPts.shift();
-      if ( this.points[2] === stepPts.at(-1).x && this.points.at(3) === stepPts.at(-1).y ) stepPts.pop();
+      if ( this.points[0] === steps2d[0].x && this.points[1] === steps2d[0].y ) steps2d.shift();
+      if ( this.points[2] === steps2d.at(-1).x && this.points.at(3) === steps2d.at(-1).y ) steps2d.pop();
       const stepPts = steps2d.flatMap(step => [step.x, step.y]);
       this.points = [this.points.slice(0, 2), ...stepPts, this.points.slice(2)]
     } else {
       steps2d.reverse();
       // Remove duplicates. Remember these are flat arrays.
-      if ( this.points.at(-2) === stepPts[0].x && this.points.at(-1) === stepPts[0].y ) stepPts.shift();
-      if ( this.points[0] === stepPts.at(-1).x && this.points.at(1) === stepPts.at(-1).y ) stepPts.pop();
+      if ( this.points.at(-2) === steps2d[0].x && this.points.at(-1) === steps2d[0].y ) steps2d.shift();
+      if ( this.points[0] === steps2d.at(-1).x && this.points.at(1) === steps2d.at(-1).y ) steps2d.pop();
       const stepPts = steps2d.flatMap(step => [step.x, step.y]);
       this.points.push(...stepPts);
     }
