79ad9924802cb5f2e67918f329bbbfb49543dd03
Michael <caewok_2000@yahoo.com>
2025-10-12 08:56:58 +0200

    Add static methods to generate spherical points

--- 3d/Sphere.js
+++ 3d/Sphere.js
@@ -1,4 +1,4 @@
-/* globals
+	/* globals
 foundry,
 PIXI,
 */
@@ -12,6 +12,8 @@ import { MatrixFlat } from "../MatrixFlat.js";
 /* Sphere
 Represent a 3d sphere, with some functions to manipulate it.
 */
+
+Math.GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
 
 export class Sphere {
 
@@ -495,6 +497,289 @@ export class Sphere {
     out.radiusSquared = Point3d.distanceSquaredBetween(this.center, a);
     out.center.set(Dx * invDetA, Dy * invDetA, Dz * invDetA);
     return out;
+  }
+  
+  /**
+   * Distribute points evenly around a sphere. 
+   * 
+   * https://stackoverflow.com/questions/9600801/evenly-distributing-n-points-on-a-sphere
+   * https://arxiv.org/pdf/0912.4540
+   * @param {number} count
+   * @returns {Point3d[]}
+   */ 
+  /*
+  static fibonacciLattice(count = 1000) {
+    const out = new Array(count);
+    const phi = Math.PI * (Math.sqrt(5) - 1); // Golden angle in radians.
+    const n = count - 1;
+    for ( let i = 0; i < count; i += 1 ) {
+      const y = 1 - (i / n) * 2; // y goes from 1 to -1.
+      const radius = Math.sqrt(1 - (y ** 2)); // Radius at y.
+      const theta = phi * i; // Golden angel increment.
+      const x = Math.cos(theta) * radius;
+      const z = Math.sin(theta) * radius;
+      out[i] = Point3d.tmp.set(x, y, z);
+    }
+    return out;
+  }
+  */
+  
+  static pointsLattice(count, ...args) {
+    // Handle small counts special. 
+    if ( count <= 0 ) return Point3d.tmp.set(0, 0, 0); // Special: Center point
+    if ( count === 1 ) return Point3d.tmp.set(0, 0, 1); // top
+    if ( count === 2 ) return [Point3d.tmp.set(0, 0, 1), Point3d.tmp.set(0, 0, -1)]; // Poles
+    if ( count < 7 ) return this.octahedron; // 3–6 points: return 6 points
+    if ( count < 9 ) return this.squareAntiprism; // 7–8 points: return 8 points
+    if ( count < 13 ) return this.icosahedron; // 9–12 points: return 12 points
+    if ( count < 21 ) return this.dodecahedron; // 13–20 points: return 20 points
+    return this.fibonacciLattice(count, ...args);
+  }
+  
+  // https://extremelearning.com.au/evenly-distributing-points-on-a-sphere/
+  static fibonacciLattice(count = 1000, e = 7/2, poles = true) {  
+    const out = new Array(count);
+    const denom = count - 1 + (2 * e);
+    for ( let i = 0; i < count; i += 1 ) {
+      const tx = (i + e) / denom;
+      const ty = i / Math.GOLDEN_RATIO;
+      
+      const theta = Math.acos(2*tx - 1) - (Math.PI / 2);
+      const phi = 2 * Math.PI * ty;
+      
+      const x = Math.cos(theta) * Math.cos(phi);
+      const y = Math.cos(theta) * Math.sin(phi);
+      const z = Math.sin(theta);
+    
+      out[i] = Point3d.tmp.set(x, y, z);
+    }
+    
+    // Define poles separately.
+    if ( poles ) {
+      out[0].set(0, 0, 1);
+      out[count - 1].set(0, 0, -1);
+    }
+    
+    return out;
+  }
+ 
+  /*
+  count = 1000;
+  pts = Sphere.fibonacciLattice(count);
+  pts = Sphere.triangleLattice(count)
+  
+  pts = fibonacciLattice4(count, 1/2, false);
+  pts = fibonacciLattice4(count, 3/2, false);
+  pts = fibonacciLattice4(count, 3/2, true);
+  pts = fibonacciLattice4(count, 7/2, false);
+  pts = fibonacciLattice4(count, 7/2, true);
+  
+  score = function(pts) {
+    maxDistArr = new Array(pts.length)
+		for ( let i = 0; i < pts.length; i += 1 ) {
+			let minDist = Number.POSITIVE_INFINITY;
+			const a = pts[i];
+			for ( let j = 0; j < pts.length; j += 1 ) {
+				if ( i === j ) continue;
+				const b = pts[j];
+				minDist = Math.min(minDist, Point3d.distanceBetween(a, b));
+			}
+			maxDistArr[i] = minDist
+		}
+    return Math.min(...maxDistArr) * Math.sqrt(pts.length)
+  }
+  
+  fn = fibonacciLattice4
+  e = 3/2
+  poles = true
+  res = {}
+  for ( const count of [25, 50, 75, 100, 200, 1000] ) {
+    const pts = fn(count, e, poles)
+    res[count] = score(pts) 
+  }
+  console.table(res)
+  */
+  
+  
+  
+  /**
+   * Diamond shape.
+   * https://en.wikipedia.org/wiki/Octahedron
+   * Evenly distributes 6 points on a sphere.
+   */
+  static get octahedron() {
+    return [
+      Point3d.tmp.set(0, 0, 1), // North pole.
+      Point3d.tmp.set(0, 0, -1), // South pole.
+      Point3d.tmp.set(1, 0, 0), // East
+      Point3d.tmp.set(-1, 0, 0), // West
+      Point3d.tmp.set(0, 1, 0), // North
+      Point3d.tmp.set(0, -1, 0), // South
+    ];
+  }
+  
+  /**
+   * Triangle sides with square caps.
+   * https://en.wikipedia.org/wiki/Square_antiprism
+   * Evenly distributes 8 points on a sphere.
+   */
+  static get squareAntiprism() {
+    return [
+      // Top square
+      Point3d.tmp.set(0, 0, 1), 
+      Point3d.tmp.set(0, 0, -1),
+      Point3d.tmp.set(1, 0, 0), 
+      Point3d.tmp.set(-1, 0, 0), // West
+      
+      // Bottom square
+      Point3d.tmp.set(0, 1, 0), // North
+      Point3d.tmp.set(0, -1, 0), // South
+      Point3d.tmp.set(0, 1, 0),
+      Point3d.tmp.set(0, 1, 0),
+    ];
+  }
+  
+  /**
+   * 20-sided die (12 points).
+   * https://en.wikipedia.org/wiki/Icosahedron
+   * Evenly distributes 12 points on a sphere.
+   */
+  static get icosahedron() {
+    const out = [
+      Point3d.tmp.set(Math.GOLDEN_RATIO, 1, 0),
+      Point3d.tmp.set(Math.GOLDEN_RATIO, -1, 0),
+      Point3d.tmp.set(-Math.GOLDEN_RATIO, 1, 0),
+      Point3d.tmp.set(-Math.GOLDEN_RATIO, -1, 0),
+      
+      Point3d.tmp.set(1, 0, Math.GOLDEN_RATIO),
+      Point3d.tmp.set(1, 0, -Math.GOLDEN_RATIO),
+      Point3d.tmp.set(-1, 0, Math.GOLDEN_RATIO),
+      Point3d.tmp.set(-1, 0, -Math.GOLDEN_RATIO),
+      
+      Point3d.tmp.set(0, Math.GOLDEN_RATIO, 1),
+      Point3d.tmp.set(0, Math.GOLDEN_RATIO, -1),
+      Point3d.tmp.set(0, -Math.GOLDEN_RATIO, 1),
+      Point3d.tmp.set(0, -Math.GOLDEN_RATIO, -1),
+    ];
+    
+    // Normalize to 1. (All points have same length here.)
+    const mult = 1 / out[0].magnitude();
+    out.forEach(pt => pt.multiplyScalar(mult, pt));
+    return out;
+  }
+  
+  /**
+   * 20-point polyhedron.
+   * https://en.wikipedia.org/wiki/Regular_dodecahedron
+   * Evenly distributes 20 points on a sphere.
+   */
+  static get dodecahedron() {
+    // See Wikipedia diagram of vertices.
+    const invGR = 1 / Math.GOLDEN_RATIO;
+    const out = [
+      // Inscribed cube
+      Point3d.tmp.set(1, 1, 1),
+      Point3d.tmp.set(1, 1, -1),
+      Point3d.tmp.set(1, -1, 1),
+      Point3d.tmp.set(1, -1, -1),
+      
+      Point3d.tmp.set(-1, 1, 1),
+      Point3d.tmp.set(-1, 1, -1),
+      Point3d.tmp.set(-1, -1, 1),
+      Point3d.tmp.set(-1, -1, -1),
+      
+      // Green vertices
+      Point3d.tmp.set(0, Math.GOLDEN_RATIO, invGR),
+      Point3d.tmp.set(0, Math.GOLDEN_RATIO, -invGR),
+      Point3d.tmp.set(0, -Math.GOLDEN_RATIO, invGR),
+      Point3d.tmp.set(0, -Math.GOLDEN_RATIO, -invGR),
+      
+      // Blue vertices
+      Point3d.tmp.set(invGR, 0, Math.GOLDEN_RATIO),
+      Point3d.tmp.set(invGR, 0, -Math.GOLDEN_RATIO),
+      Point3d.tmp.set(-invGR, 0, Math.GOLDEN_RATIO),
+      Point3d.tmp.set(-invGR, 0, -Math.GOLDEN_RATIO),
+      
+      // Pink vertices
+      Point3d.tmp.set(Math.GOLDEN_RATIO, invGR, 0),
+      Point3d.tmp.set(Math.GOLDEN_RATIO, -invGR, 0),
+      Point3d.tmp.set(-Math.GOLDEN_RATIO, invGR, 0),
+      Point3d.tmp.set(-Math.GOLDEN_RATIO, -invGR, 0),
+    ];  
+    
+    // Normalize to 1. (All points have same length here.)
+    const mult = 1 / out[0].magnitude();
+    out.forEach(pt => pt.multiplyScalar(mult, pt));
+    return out;
+  }
+  
+  /**
+   * Evenly split based on octahedron and then split the triangles to form pyramid shapes.
+   * Doesn't currently work well enough
+   * E.g., bowling pins.
+   * @param {number} maxPoints
+   * @returns {Point3d[]}
+   */
+  static triangulatedPointsLattice(minPoints = 6) {
+    if ( minPoints <= 6 ) return this.pointsLattice(minPoints);
+    
+    // Store the initial triangulation. 0 and 1 are poles. 2–5 are E, W, N, S along equator.
+    const center = Point3d.tmp.set(0, 0, 0);
+    const radius2 = 1; // 1^2 = 1
+    const pointsMap = new Map();
+    const triSet = new Set();
+    this.octahedron.forEach((pt, idx) => pointsMap.set(`${idx}`, pt));
+    
+    // Triangles are "a|b|c" where each letter is a point index.
+    // Northern hemisphere.
+    this.triSet.add("0|2|5"); // 0|W|S
+    this.triSet.add("0|5|2"); // 0|S|E
+    this.triSet.add("0|5|2"); // 0|E|N
+    this.triSet.add("0|5|2"); // 0|N|W
+    
+    // Southern hemisphere.
+    this.triSet.add("1|2|5"); // 1|W|S
+    this.triSet.add("1|5|2"); // 1|S|E
+    this.triSet.add("1|5|2"); // 1|E|N
+    this.triSet.add("1|5|2"); // 1|N|W
+    
+    const addMidpoint = (aLabel, bLabel) => {
+      const midLabel = `${aLabel}.${bLabel}`;
+      if ( pointsMap.has(midLabel) ) return midLabel;
+      
+      // Get the point between a and b. 
+      // Then extend a line from the center through that midpoint at radius length to locate new surface point.
+      const a = pointsMap.get(aLabel);
+      const b = pointsMap.get(bLabel);
+      const mid = b.subtract(a);
+      const newPt = center.towardsPointSquared(mid, radius2);
+      pointsMap.set(midLabel, newPt);
+      mid.release();
+      return midLabel;
+    }
+    
+    // Split all the triangles until reaching at least minPoints.
+    while ( pointsMap.size < minPoints ) {
+      const tris = [...triSet.values()]; // So the map can be updated.
+      for ( const tri of tris ) {
+        const [aLabel, bLabel, cLabel] = tri.split("|");
+        const abLabel = addMidpoint(aLabel, bLabel);
+        const bcLabel = addMidpoint(bLabel, cLabel);
+        const caLabel = addMidpoint(cLabel, aLabel);
+        
+        // Delete the outer triangle
+        this.triSet.delete(tri);
+        
+        // Add the new midpoint triangle
+        triSet.add(`${abLabel}|${bcLabel}|${caLabel}`); 
+        
+        // Add 3 triangles; one from each corner of the original.
+        triSet.add(`${abLabel}|${bcLabel}|${bLabel}`);
+        triSet.add(`${bcLabel}|${caLabel}|${cLabel}`);
+        triSet.add(`${caLabel}|${abLabel}|${aLabel}`);    
+      }
+    }
+    return [...pointsMap.values()]
   }
 }
 
