f06a2c5ceee59054fb4fa22005e1ccb9cbb48ac0
Michael Enion <caewok_2000@yahoo.com>
2026-04-08 13:55:48 -0700

    Add ClipperPaths unit tests

--- tests/index.js
+++ tests/index.js
@@ -4,6 +4,7 @@
 
 // Import tests
 import { registerTests as registerAABBTests } from "./AABB.test.js";
+import { registerTests as registerClipperPathsTests } from "./ClipperPaths.test.js";
 import { registerTests as registerPixelCacheTests } from "./PixelCache.test.js";
 import { registerTests as registerPointTests } from "./Point.test.js";
 import { registerTests as registerPoint3dTests } from "./Point3d.test.js";
@@ -13,6 +14,7 @@ import { registerTests as registerVerticesTests } from "./Vertices.test.js";
 
 export function registerTests(quench) {
   registerAABBTests(quench);
+  registerClipperPathsTests(quench);
   registerPixelCacheTests(quench);
   registerPointTests(quench);
   registerPoint3dTests(quench);
