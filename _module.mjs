587b5b2727f300fe6f6af2d268910552d63d1bea
Michael <caewok_2000@yahoo.com>
2026-05-11 08:26:01 +0200

    Move Marching Squares to base scripts folder

--- _module.mjs
+++ _module.mjs
@@ -27,3 +27,4 @@ export * from "./RadixSort.js";
 export * from "./Graph.js";
 export * from "./ObstacleOcclusionTest.js";
 export * as utils from "./util.js";
+export * as MarchingSquares from "./marchingsquares-esm.js";
