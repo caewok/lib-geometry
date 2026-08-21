c715efe4068b76596f69ac45115832ea979407f3
Michael Enion <caewok_2000@yahoo.com>
2026-08-21 12:51:32 -0700

    Import more performant pooling methods from HGEOM.

--- Pool.js
+++ Pool.js
@@ -10,10 +10,10 @@ Used to store temporary objects, such as points. Allows for returning objects to
 export class Pool {
 
   /** @type {number} */
-  initialSize = 10;
+  initialSize = 2048;
 
-  /** @type {Set<object>} */
-  #pool = new Set(); // Probably don't want a weak set b/c we want to reuse the object. Smaller memory if WeakSet used.
+  /** @type {object[]} */
+  #pool = []; // Faster to use an array with a flag then to use WeakSet to track objects.
 
   /** @type {class} */
   cl;
@@ -28,7 +28,14 @@ export class Pool {
 
   increasePool(n = this.initialSize) {
     const objs = this.cl.buildNObjects(n);
-    for ( let i = 0; i < n; i += 1 ) this.#pool.add(objs[i]);
+    n = objs.length; // In case this changed in buildNObjects.
+    const j = this.#pool.length;
+    this.#pool.length += n;
+    for ( let i = 0; i < n; i += 1 ) {
+      const obj = objs[i];
+      this.#pool[i + j] = obj;
+      obj._isInPool = true;
+    }
   }
 
   /**
@@ -36,12 +43,30 @@ export class Pool {
    */
   acquire() {
     // If empty, add objects to the pool.
-    if ( !this.#pool.size ) this.increasePool();
+    if ( !this.#pool.length ) this.increasePool();
 
     // Pop an object from the pool.
-    const obj = this.#pool.first();
-    this.#pool.delete(obj);
+    const obj = this.#pool.pop();
+    obj._isInPool = false;
     return obj;
+  }
+
+  /**
+   * Get multiple objects from the pool.
+   * @param {number} n Number of objects to acquire.
+   * @returns {object[]}
+   */
+  acquireMultiple(n) {
+    // Ensure the pool has enough elements
+    if (this.#pool.length < n) this.increasePool(Math.max(n - this.#pool.length, this.initialSize));
+
+    const result = new Array(n);
+    for (let i = 0; i < n; i++) {
+      const obj = this.#pool.pop();
+      obj._isInPool = false;
+      result[i] = obj;
+    }
+    return result;
   }
 
   /**
@@ -51,11 +76,15 @@ export class Pool {
   release(obj) {
     // Basic test that the object belongs.
     const cl = this.cl;
-    if ( !(obj instanceof cl) || obj.constructor.name !== cl.name ) {
+    if ( !(obj instanceof cl) ) {
       console.warn("Pool object does not match other instance in the pool.", { cl, obj });
       return;
     }
-    this.#pool.add(obj); // Important that the object here is only added once.
+
+    // Important that the object here is only added once.
+    if ( obj._isInPool ) return;
+    this.#pool.push(obj);
+    obj._isInPool = true;
   }
 
   // Use a WeakMap to store pools keyed by the Class itself.
@@ -68,9 +97,7 @@ export class Pool {
    * @returns {Pool}
    */
    static getPool(cl) {
-     if ( !this.#poolRegistry.has(cl) ) {
-       this.#poolRegistry.set(cl, new this(cl));
-     }
+     if ( !this.#poolRegistry.has(cl) ) this.#poolRegistry.set(cl, new this(cl));
      return this.#poolRegistry.get(cl);
    }
 }
@@ -93,6 +120,19 @@ export const PoolableMixin = superclass => class extends superclass {
   static get tmp() { return this.pool.acquire(); }
 
   /**
+   * Eventual replacement for tmp getter.
+   * @returns {object}
+   */
+  static create() { return this.pool.acquire(); }
+
+  /**
+   * Method for creating multiple objects, using the pool.
+   * @param {number} n
+   * @returns {object[]}
+   */
+  static createN(n) { return this.pool.acquireMultiple(n); }
+
+  /**
    * Release an instance back to the pool and trigger cleanup.
    * @param {Poolable} objs
    */
@@ -102,6 +142,10 @@ export const PoolableMixin = superclass => class extends superclass {
   }
 
   static release(...objs) { objs.forEach(obj => this._release(obj)); }
+
+  // Flag to track pool status.
+  // Added here so the object class is not later modified.
+  _isInPool = false;
 
   /**
    * Trigger automatic return to the pool if the point is defined with a "using" declaration.
@@ -120,8 +164,12 @@ export const PoolableMixin = superclass => class extends superclass {
    * @returns {Poolable[n]}
    */
   static buildNObjects(n) {
-    return Array.from({ length: n}, () => new this());
+    const arr = new Array(n);
+    for ( let i = 0; i < n; i += 1 ) arr[i] = new this();
+    return arr;
   }
+
+
 }
 
 /**
@@ -130,11 +178,8 @@ export const PoolableMixin = superclass => class extends superclass {
  * Tracks contiguous blocks of empty space and allocates accordingly.
  */
 export class BufferManager {
-  /** @type {ArrayBuffer} */
-  buffer;
-
-  /** @type {object[]} */
-  freeSegments = [];
+  /** @type {Mat<ArrayBuffer, Array<{byteOffset, byteSize}>} */
+  freeSegmentsMap = new Map();
 
   /** @type {TypedArray} */
   typedClass;
@@ -142,11 +187,31 @@ export class BufferManager {
   /** @type {number} */
   get bytesPerElement() { return this.typedClass.BYTES_PER_ELEMENT; }
 
-  constructor(totalSize = 0, { typedClass = Float32Array, maxSize = totalSize } = {}) {
+  /** @type {number} */
+  maxBufferSize = 0;
+
+  /** @type {ArrayBuffer} */
+  currentBuffer;
+
+  constructor(totalSize = 0, { typedClass = Float32Array, maxBufferSize = totalSize } = {}) {
     this.typedClass = typedClass;
-    const byteSize = totalSize * this.bytesPerElement;
-    this.buffer = new ArrayBuffer(byteSize, { maxByteLength: maxSize * this.bytesPerElement });
-    this.freeSegments.push({ byteOffset: 0, byteSize });
+    this.maxBufferSize = maxBufferSize;
+    this._addNewBuffer(totalSize);
+  }
+
+  /**
+   * Add a new buffer of a given size.
+   * @param {number} size
+   * @returns {ArrayBuffer}
+   */
+  _addNewBuffer(size, maxBufferSize) {
+    const byteSize = size * this.bytesPerElement;
+    maxBufferSize ??= this.maxBufferSize;
+    maxBufferSize = Math.max(size, maxBufferSize);
+    const buffer = new ArrayBuffer(byteSize, { maxByteLength: maxBufferSize * this.bytesPerElement });
+    this.freeSegmentsMap.set(buffer, [{ byteOffset: 0, byteSize }]);
+    this.currentBuffer = buffer;
+    return buffer;
   }
 
   /**
@@ -155,47 +220,66 @@ export class BufferManager {
    * @returns {TypedArray<size>}
    */
   newArray(size) {
-    const byteOffset = this.allocate(size);
-    return new this.typedClass(this.buffer, byteOffset, size);
+    const { buffer, byteOffset } = this.allocate(size);
+    return new this.typedClass(buffer, byteOffset, size);
   }
 
   /**
    * Reserve a block of space.
    * If out of space, constructs a new buffer.
    * @param {number} size       Number of elements to reserve
-   * @returns {number} The byte offset.
+   * @returns {object}
+   * - @prop {ArrayBuffer} buffer   The buffer to use
+   * - @prop {number} bytOffset     The byte offset.
    */
   allocate(size) {
     const byteSize = size * this.bytesPerElement;
-    for ( let i = 0, iMax = this.freeSegments.length; i < iMax; i += 1 ) {
-      const segment = this.freeSegments[i];
+
+    // Search all buffers for a free segment.
+    for ( const [buffer, segments] of this.freeSegmentsMap ) {
+      const byteOffset = this._findSegmentWithFreeSpace(segments, byteSize);
+      if ( byteOffset !== null ) return { buffer, byteOffset };
+    }
+
+    // Insufficient memory left in the buffer.
+    // Try to grow the most recently added buffer.
+    const lastBuffer = this.currentBuffer;
+    const totalBytesNeeded = lastBuffer.byteLength + byteSize;
+    if ( lastBuffer.maxByteLength >= totalBytesNeeded ) {
+      // Grow the buffer and use the resized portion for this allocation.
+      const byteOffset = lastBuffer.byteLength;
+      lastBuffer.resize(totalBytesNeeded);
+      return { buffer: lastBuffer, byteOffset };
+    }
+
+    // Last resort: Add a new buffer.
+    const buffer = this._addNewBuffer(Math.max(size,  2 ** 10));
+    const segments = this.freeSegmentsMap.get(buffer);
+    this._findSegmentWithFreeSpace(segments, byteSize); // Must still process the segment.
+    return { buffer, byteOffset: 0 }; // By definition, because we just added it, offset is 0.
+  }
+
+  /**
+   * Identify a segment with sufficient space.
+   * @param {Array<{byteOffset, byteSize}>} segments      Memory segments to check
+   * @param {number} byteSize                             Target size
+   * @returns {object<byteOffset, byteSize>|null} The first segment with sufficient space
+   */
+  _findSegmentWithFreeSpace(segments, byteSize) {
+    for ( let i = 0, n = segments.length; i < n; i += 1 ) {
+      const segment = segments[i];
       if ( segment.byteSize >= byteSize ) {
         const byteOffset = segment.byteOffset;
-        if ( segment.byteSize === byteSize ) this.freeSegments.splice(i, 1); // Perfect fit: remove the segment entirely.
+        if ( segment.byteSize === byteSize ) segments.splice(i, 1); // Perfect fit: remove the segment entirely.
         else {
-          // Partial fit: shrink the existing fre segment.
+          // Partial fit: shrink the existing free segment.
           segment.byteOffset += byteSize;
           segment.byteSize -= byteSize;
         }
         return byteOffset;
       }
     }
-
-    // Insufficient memory left in the buffer.
-    // Expand buffer if possible.
-    const totalBytesNeeded = this.buffer.byteLength + byteSize;
-    if ( this.buffer.maxByteLength > totalBytesNeeded ) {
-      // Grow the buffer and use the resized portion for this allocation.
-      const byteOffset = this.buffer.byteLength;
-      this.buffer.resize(totalBytesNeeded);
-      return byteOffset;
-    } else {
-      // Trash the buffer and start anew.
-      this.freeSegments.length = 1;
-      this.freeSegments[0] = { byteOffset: 0, byteSize: this.buffer.byteLength };
-      this.buffer = new ArrayBuffer(Math.max(this.buffer.byteLength, byteSize), { maxByteLength: Math.max(this.buffer.maxByteLength, byteSize) });
-      return this.allocate(size);
-    }
+    return null;
   }
 
   /**
@@ -204,30 +288,32 @@ export class BufferManager {
    */
   release(arr) {
     arr.fill(0); // Good practice to limit caching errors.
-    if ( arr.buffer !== this.buffer ) return;
-    const byteSize = arr.byteLength;
-    const byteOffset = arr.byteOffset;
-    const newSegment = { byteSize, byteOffset };
+    if ( !this.freeSegmentsMap.has(arr.buffer) ) return;
+
+    const segments = this.freeSegmentsMap.get(arr.buffer);
+    const newSegment = { byteSize: arr.byteLength, byteOffset: arr.byteOffset };
 
     // Insert and maintain sorted order by offset to allow merging.
-    const idx = this.freeSegments.findIndex(s => s.byteOffset > byteOffset);
-    if ( ~idx ) this.freeSegments.splice(idx, 0, newSegment);
-    else this.freeSegments.push(newSegment);
-    this._mergeNeighbors();
+    const idx = segments.findIndex(s => s.byteOffset > newSegment.byteOffset);
+    if ( ~idx ) segments.splice(idx, 0, newSegment);
+    else segments.push(newSegment);
+
+    // Combine segments that have empty space at their respective borders.
+    this._mergeNeighbors(segments);
   }
 
   /**
    * Combines adjacent free blocks to limit fragmentation.
    */
-  _mergeNeighbors() {
-    for ( let i = 0, iMax = this.freeSegments.length - 1; i < iMax; i += 1 ) {
-      const current = this.freeSegments[i];
-      const next = this.freeSegments[i+1];
+  _mergeNeighbors(segments) {
+    for ( let i = 0, iMax = segments.length - 1; i < iMax; i += 1 ) {
+      const current = segments[i];
+      const next = segments[i+1];
 
       // If current block ends exactly where the next starts, merge.
       if ( current.byteOffset + current.byteSize === next.byteOffset ) {
         current.byteSize += next.byteSize;
-        this.freeSegments.splice(i + 1, 1);
+        segments.splice(i + 1, 1);
         iMax--;
         i--; // Check again with the newly merged block.
       }
